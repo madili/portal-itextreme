2572b68c920c305a9e916f76c94e0d167b7c39b6
Marcos Dioni Lima <mlima@vix.itextreme.com.br>
2017-01-12 15:29:50 -0200

    fisrt commit

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Solution()
+        {
+            return View();
+        }
+
+        public IActionResult Lifestyle()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
